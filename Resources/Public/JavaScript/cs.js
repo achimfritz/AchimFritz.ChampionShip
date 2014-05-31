eaa9da4b94a62d94f7529a8ea7e0c7d59e5b6b23
Achim Fritz <af@lightwerk.com>
2014-05-31 11:42:58 +0200

    [TASK] fullscreen for tipGroupResults

--- Resources/Public/JavaScript/cs.js
+++ Resources/Public/JavaScript/cs.js
@@ -1,25 +1,22 @@
 (function ($) {
 	$(function () {
+		// datepicker
 		$('.datetimepicker').datetimepicker({
 			timeformat: 'H:i',
 			dateFormat: 'dd.mm.yy',
 			stepMinute: 5
 		});
 
+		// equal height
 		$('div.cs-col').equalHeight();
-
-		$('ul.nav li a').on('shown', function() {
-			//$('div.cs-col').equalHeight();
-		});
 
 		// delete action
 		$('span.deleteAction').bind('click', function() {
 			var form = $(this).next('form.deleteAction');
 			$('input.btn', form).trigger('click');
 		});
-				//form.trigger('submit');
-				//ev.preventDefault();
 
+		// data tables
 		var oTable = $('.dataTable').dataTable({
           "bJQueryUI": true,
           "aLengthMenu": [ 20, 50, 200 ],
@@ -27,6 +24,23 @@
           'iDisplayLength': 20
           //'sDom': 'lfrtip'
      });
+
+	  // fullscreen
+	  $('#fullscreen').bind('click', function(ev) {
+			if ($(this).hasClass('full')) {
+				$('.cs-main').addClass('span8');
+				$('.cs-main').removeClass('span12');
+				$('.cs-right').show();
+				$('.cs-menu').show();
+				$(this).removeClass('full');
+			} else {
+				$('.cs-main').removeClass('span8');
+				$('.cs-main').addClass('span12');
+				$('.cs-right').hide();
+				$('.cs-menu').hide();
+				$(this).addClass('full');
+			}
+	  });
 	});
 }(jQuery));
 
