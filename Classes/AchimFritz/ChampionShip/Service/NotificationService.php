f519ea9393ca3127379f28927df34f8823a233c7
Achim Fritz <af@achimfritz.de>
2021-05-18 16:51:42 +0200

    TASK: Apply migration TYPO3.Flow-20161115140430
    Adjust to the renaming of the Object namespace in Flow 4.0

--- Classes/AchimFritz/ChampionShip/Service/NotificationService.php
+++ Classes/AchimFritz/ChampionShip/Service/NotificationService.php
@@ -28,7 +28,7 @@ class NotificationService
     protected $settings;
 
     /**
-     * @var \TYPO3\Flow\Object\ObjectManagerInterface
+     * @var \TYPO3\Flow\ObjectManagement\ObjectManagerInterface
      * @Flow\Inject
      */
     protected $objectManager;
